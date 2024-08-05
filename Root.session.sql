97dc1a7848dcb601660db5484bc47d08711d6043
adityasd314 <adityasd314@gmail.com>
2024-08-05 16:01:23 +0530

    first commit DBMS Assign 1

---
+++ Root.session.sql
@@ -0,0 +1,10 @@
+select * from university.classroom;
+select * from university.department;
+SELECT * FROM university.course;
+SELECT * FROM university.instructor;
+SELECT * FROM university.section;
+SELECT * FROM university.teaches;
+SELECT * FROM university.student;
+-- 	PRIMARY KEY (ID),
+-- 	FOREIGN KEY (ID) REFERENCES instructor (ID) ON DELETE CASCADE
+-- );
